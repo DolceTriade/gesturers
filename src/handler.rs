9ea245ee629941b99140f761af03182fe10765e0
Harsh Modi <harshmodi@google.com>
2019-02-28 21:22:16 -0800

    handler: Add handler.rs missed in last commit.

---
+++ src/handler.rs
@@ -0,0 +1,62 @@
+use circgr::classifier::Classifier;
+use circgr::gesture::Gesture;
+use crossbeam::channel;
+use std::path::{Path, PathBuf};
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub enum Mode {
+    Record(String),
+    Recognize,
+}
+
+pub struct Handler {
+    mode: Mode,
+    gesture_listener: channel::Receiver<Gesture>,
+    classifier: Classifier,
+    template_path: PathBuf,
+}
+
+impl Handler {
+    pub fn new(mode: Mode, listener: channel::Receiver<Gesture>, template_path: &Path) -> Self {
+        let mut classifier = Classifier::new();
+        if mode == Mode::Recognize {
+            let dir = std::fs::read_dir(&template_path)
+                .expect(&format!("Error reading {:?}", &template_path));
+            for entry_or in dir {
+                let entry = entry_or.expect("Error reading entry");
+                let bytes = std::fs::read(entry.path())
+                    .expect(&format!("Error reading {:?}", entry.path()));
+                let gesture_or: bincode::Result<Gesture> = bincode::deserialize(&bytes);
+                match gesture_or {
+                    Ok(gesture) => classifier.add_template(gesture),
+                    Err(err) => {
+                        eprintln!("Error decoding gesture in {:?}: {:?}", entry.path(), err)
+                    }
+                }
+            }
+        }
+        Handler {
+            mode: mode,
+            gesture_listener: listener,
+            classifier: classifier,
+            template_path: template_path.to_path_buf(),
+        }
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            let gesture = &self.gesture_listener.recv().unwrap();
+            match &self.mode {
+                Mode::Record(name) => {
+                    let mut path = std::path::PathBuf::new();
+                    path.push(&self.template_path);
+                    path.push(name);
+                    std::fs::write(path.as_path(), bincode::serialize(&gesture).unwrap()).unwrap();
+                }
+                _ => {
+                    println!("Doing stuff: {:?}", &self.classifier.classify(&gesture));
+                }
+            }
+        }
+    }
+}
